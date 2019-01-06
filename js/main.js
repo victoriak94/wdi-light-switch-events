0569d0c41476b1ab373b369d5d77e7fbb431faab
victoriak94 <vkamila6@gmail.com>
2019-01-05 19:05:43 -0500

    Select status text by class name and save it in statusText variable. Do same for lightswitch and light. Add click event listener for lightswitch. Add event listener for entire document to ensure entire DOM is loaded before lightswitch function can run. Complete assignment

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
 
+  const statusText = document.getElementsByClassName('status');
+  const lightswitch = document.getElementByTagName('button');
+  const light = document.getElementsByClassName('light');
 
+  lightswitch.addEventListener('click', function() {
+    if (lightswitch.className === 'switch off') {
+      lightswitch.className = 'switch on';
+      light.className = 'light';
+      statusText.innerText = "It's so bright in here!";
+    } else if (lightswitch.className === 'switch on') {
+      lightswitch.className = 'switch off';
+      light.className = 'dark';
+      statusText.innertext = 'Hey, who turned off the lights?';
+    }
+  });
+});
